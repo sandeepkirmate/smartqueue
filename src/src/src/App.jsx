09834bd522443f57ce4e01d12960dc40072c8e6b
sandeepkirmate <sandeepkirmate@gmail.com>
2026-06-26 14:54:36 +0530

    Create App.jsx

---
+++ src/src/src/App.jsx
@@ -0,0 +1,26 @@
+export default function App() {
+  return (
+    <div className="container">
+      <h1>💈 Shiv Hair Salon</h1>
+
+      <p className="tagline">SmartQueue System</p>
+
+      <div className="card">
+        <h2>🟢 Now Serving</h2>
+        <h1>Token #08</h1>
+      </div>
+
+      <div className="card">
+        <h2>⏱ Waiting Time</h2>
+        <h1>25 Minutes</h1>
+      </div>
+
+      <button className="btn">🎫 Join Live Queue</button>
+
+      <div className="footer">
+        <p>📞 73500 33608</p>
+        <p>🕘 9 AM - 10 PM | Saturday Off</p>
+      </div>
+    </div>
+  );
+}
